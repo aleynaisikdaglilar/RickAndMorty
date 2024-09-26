37af2d859f1171e0bbeb7ae0866bf3b9446a051f
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-09-27 01:48:09 +0300

    Compositional Layout

--- RickAndMorty/Views/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Views/Other/RMCharacterDetailViewController.swift
@@ -9,15 +9,16 @@ import UIKit
 
 ///     Controller to show info about single character
 final class RMCharacterDetailViewController: UIViewController {
-
+    
     private let viewModel: RMCharacterDetailViewViewModel
     
-    private let detailView = RMCharacterDetailView()
+    private let detailView: RMCharacterDetailView
     
-//    MARK: Init
+    //    MARK: Init
     
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -25,7 +26,7 @@ final class RMCharacterDetailViewController: UIViewController {
         fatalError("Unsupported")
     }
     
-//    MARK: - Lifecycle
+    //    MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +35,14 @@ final class RMCharacterDetailViewController: UIViewController {
         view.addSubview(detailView)
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
         addConstraints()
+        
+        detailView.collectionView?.delegate = self
+        detailView.collectionView?.dataSource = self
     }
-
+    
     @objc
     private func didTapShare() {
-//        Share character info
+        //        Share character info
     }
     
     private func addConstraints() {
@@ -50,4 +54,32 @@ final class RMCharacterDetailViewController: UIViewController {
         ])
         
     }
+}
+
+// MARK: CollectionView
+
+extension RMCharacterDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemPink
+        } else  if indexPath.section == 1 {
+            cell.backgroundColor = .systemGreen
+        } else {
+            cell.backgroundColor = .systemBlue
+        }
+        return cell
+    }
+    
+    
 }
