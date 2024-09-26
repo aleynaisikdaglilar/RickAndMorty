ccd27da240967076cc1ce5b96bf5376fa70547a7
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-09-26 15:33:47 +0300

    Character Detail View

--- RickAndMorty/Views/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Views/Other/RMCharacterDetailViewController.swift
@@ -12,6 +12,10 @@ final class RMCharacterDetailViewController: UIViewController {
 
     private let viewModel: RMCharacterDetailViewViewModel
     
+    private let detailView = RMCharacterDetailView()
+    
+//    MARK: Init
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -27,6 +31,23 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+        addConstraints()
     }
 
+    @objc
+    private func didTapShare() {
+//        Share character info
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            detailView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
 }
