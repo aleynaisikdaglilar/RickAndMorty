722914a2f0ba0750d9fefa9cd9a263dfd5a066ce
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-10-03 11:44:49 +0300

    API Cach Layer

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -13,6 +13,8 @@ final class RMService {
     /// Shared Singleton instance
     static let shared = RMService()
     
+    private let cacheManager = RMAPICacheManager()
+    
     /// Privatized constructor
     private init() {}
     
@@ -31,19 +33,31 @@ final class RMService {
         expecting type: T.Type,
         completion: @escaping (Result<T, Error>) -> Void
     ) {
+        if let cachedData = cacheManager.cachedResponse(for: request.endpoint, url: request.url) {
+            print("Using cache api response")
+            do {
+                let result = try JSONDecoder().decode(type.self, from: cachedData)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+            return
+        }
+        
         guard let urlRequest = self.request(from: request) else {
             completion(.failure(RMServiceError.failedToCreateRequest))
             return
         }
-        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? RMServiceError.failedToData))
                 return
             }
             
-//            Decode response
+            //            Decode response
             do {
                 let result = try JSONDecoder().decode(type.self, from: data)
+                self?.cacheManager.setCache(for: request.endpoint, url: request.url, data: data)
                 completion(.success(result))
             } catch {
                 completion(.failure(error))
@@ -52,7 +66,7 @@ final class RMService {
         task.resume()
     }
     
-//    MARK - Private
+    //    MARK - Private
     
     private func request(from rmRequest: RMRequest) -> URLRequest? {
         guard let url = rmRequest.url else {
---
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Aleyna Işıkdağlılar on 3.10.2024.
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+}
