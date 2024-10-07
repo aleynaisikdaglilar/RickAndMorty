2cc18ddd04d143c0a0ccaf900528356acd72e709
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-10-07 13:42:15 +0300

    Episode Detail Layout

---
+++ RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeInfoCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RMEpisodeInfoCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Aleyna Işıkdağlılar on 7.10.2024.
+//
+
+import Foundation
+
+struct RMEpisodeInfoCollectionViewCellViewModel {
+    public let title: String
+    public let value: String
+}
---
+++ RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMEpisodeInfoCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Aleyna Işıkdağlılar on 7.10.2024.
+//
+
+import UIKit
+
+final class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
+    
+}
