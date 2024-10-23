0da86654dfe141c04043cf171bd60e4dceef8fb4
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-10-23 16:38:28 +0300

    Search Request Builder

--- RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -16,8 +16,8 @@ final class RMSearchViewViewModel {
     let config: RMSearchViewController.Config
     private var optionMap: [RMSearchInputViewViewModel.DynamicOption: String] = [:]
     private var searchText = ""
-    
     private var optionMapUpdateBlock: (((RMSearchInputViewViewModel.DynamicOption,String)) -> Void)?
+    private var searchResultHandler: (() -> Void)?
     
     //    MARK: - Init
     
@@ -27,14 +27,43 @@ final class RMSearchViewViewModel {
     
     //    MARK: - Public
     
+    public func registerSearchResultHandler(_ block: @escaping() -> Void) {
+        self.searchResultHandler = block
+    }
+    
     public func executeSearch() {
-//        create request based on filters
-//        send api call
-//        notify view of results, no results or error
+//        Test search text
+        searchText = "Rick"
+        
+//        Build arguments
+        var queryParams: [URLQueryItem] = [URLQueryItem(name: "name", value: searchText)]
+        
+        //        Add options
+        queryParams.append(contentsOf: optionMap.enumerated().compactMap({ _, element in
+            let key: RMSearchInputViewViewModel.DynamicOption = element.key
+            let value: String = element.value
+            return URLQueryItem(name: key.queryArgument, value: value)
+        }))
+        
+//        Create request
+        let  request = RMRequest(endpoint: config.type.endpoint, queryParameters: queryParams)
+        
+        print(request.url?.absoluteString)
+        
+        RMService.shared.execute(request, expecting: RMGetAllCharactersResponse.self) { result in
+            //        notify view of results, no results or error
+            
+            switch result {
+            case .success(let model):
+                print(model.results.count)
+            case .failure:
+                break
+            }
+        }
     }
     
     public func set(query text: String) {
-        self.searchText = text  
+        self.searchText = text
     }
     
     public func set(value: String, for option: RMSearchInputViewViewModel.DynamicOption) {
