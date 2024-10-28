40eeff3b4ca6aac88587ca5c37f8279e1f7ee33b
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-10-28 23:06:03 +0300

    Location Pagination Update + Devices

--- RickAndMorty/Views/Location/RMLocationView.swift
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -22,6 +22,15 @@ final class RMLocationView: UIView {
             UIView.animate(withDuration: 0.3) {
                 self.tableView.alpha = 1
             }
+            
+            viewModel?.registerDidFinishPaginationBlock { [weak self] in
+                DispatchQueue.main.async {
+                    //                Loading indicator go bye bye
+                    self?.tableView.tableFooterView = nil
+                    //                Reload data
+                    self?.tableView.reloadData()
+                }
+            }
         }
     }
     
@@ -137,11 +146,6 @@ extension RMLocationView: UIScrollViewDelegate {
                     self?.showLoadingIndicator()
                 }
                 viewModel.fetchAdditionalLocations()
-                
-                DispatchQueue.main.asyncAfter(deadline: .now()+3, execute: {
-                    print("refresh")
-                    self?.tableView.reloadData()
-                })
             }
             t.invalidate()
         }
