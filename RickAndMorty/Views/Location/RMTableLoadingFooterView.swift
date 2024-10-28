df1811256538d3174282eb8606b8cbba75b15dae
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-10-28 16:53:22 +0300

    Location Pagination

---
+++ RickAndMorty/Views/Location/RMTableLoadingFooterView.swift
@@ -0,0 +1,41 @@
+//
+//  RMTableLoadingFooterView.swift
+//  RickAndMorty
+//
+//  Created by Aleyna Işıkdağlılar on 28.10.2024.
+//
+
+import UIKit
+
+final class RMTableLoadingFooterView: UIView {
+    
+    private let spinner: UIActivityIndicatorView = {
+       let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(spinner)
+        spinner.startAnimating()
+        
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+        
+    }
+}
