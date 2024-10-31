e6d62149adeaa7ab118323a932a9a64352035212
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-10-31 17:37:38 +0300

    Fixing Bugs

--- RickAndMorty/Views/Location/RMLocationView.swift
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/// Interface to relay location view events
 protocol RMLocationViewDelegate: AnyObject {
     func rmLocationView(_ locationView: RMLocationView, didSelect location: RMLocation)
 }
@@ -90,6 +91,8 @@ final class RMLocationView: UIView {
     }
 }
 
+//MARK: - UITableViewDelegate
+
 extension RMLocationView: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -105,6 +108,8 @@ extension RMLocationView: UITableViewDelegate {
         delegate?.rmLocationView(self, didSelect: locationModel)
     }
 }
+
+// MARK: - UITableViewDataSource
 
 extension RMLocationView: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -128,6 +133,8 @@ extension RMLocationView: UITableViewDataSource {
     }
 }
 
+//MARK: - UIScrollViewDelegate
+
 extension RMLocationView: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard let viewModel = viewModel,
@@ -142,9 +149,7 @@ extension RMLocationView: UIScrollViewDelegate {
             let totalScroolViewFixedHeight = scrollView.frame.size.height
             
             if offset >= (totalContentHeight - totalScroolViewFixedHeight - 120) {
-                DispatchQueue.main.async {
-                    self?.showLoadingIndicator()
-                }
+                self?.showLoadingIndicator()
                 viewModel.fetchAdditionalLocations()
             }
             t.invalidate()
